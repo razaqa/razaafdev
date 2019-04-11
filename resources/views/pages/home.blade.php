4766f6833d40ada8e5d0f4453313615a79d56d04
Dhafin <dhafins18@gmail.com>
2019-04-11 11:51:15 +0700

    add animation text to cover and edit cover section

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -10,24 +10,19 @@
 @section('content')
     <div class="content">
         <div id="cover" class="uk-inline img-cover-height uk-height-large uk-background-cover uk-overflow-hidden uk-flex uk-flex-center" uk-parallax="opacity: 1, 0;" style="background-image: url('img/HomeCover.jpg');">
-            <div class="uk-width-1-2@s uk-text-center uk-margin-auto uk-margin-auto-vertical headline">
+            <div class="uk-width-3-4@s uk-text-center uk-margin-auto uk-margin-auto-vertical">
                 <div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1" uk-slideshow="animation: scale; autoplay: true; autoplay-interval: 4000;">
-                    <ul class="uk-slideshow-items">
+                    <ul class=" uk-slideshow-items uk-flex uk-flex-center">
                         <li>
-                            <h1 class="white uk-width-1-2@s uk-text-center uk-margin-auto uk-margin-auto-vertical headline">creativity.</h1>
+                            <canvas id="canvas-cover"></canvas>
                         </li>
                         <li>
-                            <h1 class="white uk-width-1-2@s uk-text-center uk-margin-auto uk-margin-auto-vertical headline">curiosity.</h1>
+                            <h1 class="white uk-text-center uk-margin-auto uk-margin-auto-vertical headline">Hello. My name is Razaqa Dhafin Haffiyan</h1>
                         </li>
                         <li>
-                            <h1 class="white uk-width-1-2@s uk-text-center uk-margin-auto uk-margin-auto-vertical headline">critical.</h1>
-                        </li>
-                        <li>
-                            <h1 class="white uk-width-1-2@s uk-text-center uk-margin-auto uk-margin-auto-vertical headline">courage.</h1>
+                            <h1 class="white uk-text-center uk-margin-auto uk-margin-auto-vertical headline">Welcome! Feel free to see all my works here..</h1>
                         </li>
                     </ul>
-                    <!-- <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slideshow-item="previous"></a>
-                    <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slideshow-item="next"></a> -->
                 </div>
             </div>
             <a class="uk-position-bottom-center slide-down uk-button uk-button-primary" href="#writings" uk-scroll>
